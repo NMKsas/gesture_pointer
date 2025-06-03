788d7d01f05999e2f0f0553f19f61770a4d60070
nmksas <noora.sassali@gmail.com>
2025-06-03 09:34:50 +0300

    Commonly used constants into one file

---
+++ ros1env/src/gesture_pointer/src/gesture_pointer/constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# generic constants 
+DEFAULT_PATH = "/up/ros1env/src/gesture_pointer/data"
+CACHE_ENABLED = True
+DEBUG_ENABLED = True
+
+CORNER_CACHE_FILE = 'corner_cache'
+MASK_CACHE_FILE = 'mask_cache'
+
+# ArUco data collection constants 
+CAMERA_TF_FRAME = 'st_cam_color_optical_frame'
+CORNERS_FILE_NAME = 'corners_mean_.csv'
+MARKERS = [100,101,102,103] 
+
--- ros1env/src/gesture_pointer/src/gesture_pointer/utils/__init__.py
+++ ros1env/src/gesture_pointer/src/gesture_pointer/utils/__init__.py
@@ -3,5 +3,4 @@ from utils.pose_utils import point_to_workspace_plane
 from utils.visualization_utils import generate_marker_from_point, \
                                       generate_line_strip_marker
 
-from utils.file_utils import read_corners, \
-                             generate_csv
+from utils.file_utils import read_corners, generate_csv
